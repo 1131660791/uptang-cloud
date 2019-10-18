9cb8e8f4c12d75dd0a1c227459a87149de1504d3
jiang.chuan <jc@uptang.com.cn>
2019-10-18 17:43:59 +0800

    refactor: 增加context-path

--- uptang-cloud-api/uptang-cloud-base-parent/uptang-cloud-base/src/main/java/com/uptang/cloud/base/controller/AttachmentController.java
+++ uptang-cloud-api/uptang-cloud-base-parent/uptang-cloud-base/src/main/java/com/uptang/cloud/base/controller/AttachmentController.java
@@ -122,10 +122,11 @@ public class AttachmentController extends BaseController implements AttachmentPr
             @ApiImplicitParam(name = "mode", value = "拼接模式（horizontally:横拼, vertically:竖拼）", paramType = "query", defaultValue = "vertically")
     })
     @GetMapping(path = "/{examCode}/{subjectCode}/{itemNum}/{studentId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ApiOut<String> getImageUrl(@NotBlank @PathVariable String examCode, @NotBlank @PathVariable String subjectCode,
-                                      @NotBlank @PathVariable String itemNum, @NotBlank @PathVariable String studentId,
-                                      @RequestParam(name = "mode", defaultValue = "vertically", required = false) String mode) {
-        return ApiOut.newSuccessResponse(processor.generateUrlPath(examCode, subjectCode, itemNum, studentId, "vertically".equalsIgnoreCase(mode)));
+    public ApiOut<String[]> getImageUrl(@NotBlank @PathVariable String examCode, @NotBlank @PathVariable String subjectCode,
+                                        @NotBlank @PathVariable String itemNum, @NotBlank @PathVariable String studentId,
+                                        @RequestParam(name = "mode", defaultValue = "vertically", required = false) String mode) {
+        String relativePath = processor.generateUrlPath(examCode, subjectCode, itemNum, studentId, "vertically".equalsIgnoreCase(mode));
+        return ApiOut.newSuccessResponse(new String[]{attachmentService.generateFullUrl(relativePath), relativePath});
     }
 
 
@@ -139,32 +140,32 @@ public class AttachmentController extends BaseController implements AttachmentPr
      * @param studentId    学生ID或准考证号
      * @param mode         拼接模式（horizontally:横拼, vertically:竖拼）
      * @param imageSources <pre>
-     *   [{
-     *     "height": 476,
-     *     "path": "/21/20191010/7/7_18120190918114540270_a.jpg",
-     *     "width": 1427,
-     *     "x": 110,
-     *     "y": 1297
-     *   }, {
-     *     "height": 458,
-     *     "path": "/21/20191010/7/7_18120190918114540270_b.jpg",
-     *     "width": 1409,
-     *     "x": 116,
-     *     "y": 161
-     *   }, {
-     *     "height": 1101,
-     *     "path": "/21/20191010/7/7_18120190918114540270_b.jpg",
-     *     "width": 1398,
-     *     "x": 131,
-     *     "y": 645
-     *   }, {
-     *     "height": 1132,
-     *     "path": "/21/20191010/7/7_18120190918114540270_b.jpg",
-     *     "width": 1430,
-     *     "x": 111,
-     *     "y": 641
-     *   }]
-     * </pre>
+     *                       [{
+     *                         "height": 476,
+     *                         "path": "/21/20191010/7/7_18120190918114540270_a.jpg",
+     *                         "width": 1427,
+     *                         "x": 110,
+     *                         "y": 1297
+     *                       }, {
+     *                         "height": 458,
+     *                         "path": "/21/20191010/7/7_18120190918114540270_b.jpg",
+     *                         "width": 1409,
+     *                         "x": 116,
+     *                         "y": 161
+     *                       }, {
+     *                         "height": 1101,
+     *                         "path": "/21/20191010/7/7_18120190918114540270_b.jpg",
+     *                         "width": 1398,
+     *                         "x": 131,
+     *                         "y": 645
+     *                       }, {
+     *                         "height": 1132,
+     *                         "path": "/21/20191010/7/7_18120190918114540270_b.jpg",
+     *                         "width": 1430,
+     *                         "x": 111,
+     *                         "y": 641
+     *                       }]
+     *                     </pre>
      * @throws Exception 生成图片的异常
      */
     @ApiOperation(value = "处理图片 裁切/接接", response = Void.class)
--- uptang-cloud-task/uptang-cloud-task-paper-image/src/main/java/com/uptang/cloud/task/PaperImageTask.java
+++ uptang-cloud-task/uptang-cloud-task-paper-image/src/main/java/com/uptang/cloud/task/PaperImageTask.java
@@ -1,7 +1,12 @@
 package com.uptang.cloud.task;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.obs.services.ObsClient;
+import com.uptang.cloud.base.common.support.PaperImageProcessor;
 import com.uptang.cloud.task.util.CacheKeys;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,6 +18,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.io.File;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.stream.IntStream;
 
 /**
  * 试卷图片处理
@@ -32,13 +45,75 @@ public class PaperImageTask implements CommandLineRunner {
         SpringApplication.run(PaperImageTask.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        sendTasks();
+        // uploadTestFiles();
+    }
+
+    // ====================== 以下是模拟发布考试 ======================
+    private void sendTasks() {
+        String cacheKey = CacheKeys.getExamExtractTaskKey();
+        redisTemplate.opsForSet().add(cacheKey, "xty_20191011112438446");
+        // redisTemplate.opsForSet().add(cacheKey, "xty_20190927140900970", "xty_20190617150608344", "xty_20191011112438446", "xty_20190927140900970", "uptang_base", "xty");
+        //redisTemplate.opsForSet().add(cacheKey,  "uptang_base", "xty");
+    }
+
+
+    // ====================== 以下是测试批量上传图片 ======================
     @Autowired
     private StringRedisTemplate redisTemplate;
 
-    @Override
-    public void run(String... args) throws Exception {
-        String cacheKey = CacheKeys.getExamExtractTaskKey();
-        redisTemplate.opsForSet().add(cacheKey, "xty_20190617150608344", "xty_20191011112438446", "xty_20190927140900970", "uptang_base", "xty");
-        //redisTemplate.opsForSet().add(cacheKey,  "uptang_base", "xty");
+    @Autowired
+    private PaperImageProcessor processor;
+
+
+    private void uploadTestFiles() {
+        final File[] files = new File("D:\\big-data\\images\\2").listFiles();
+        if (ArrayUtils.isEmpty(files)) {
+            return;
+        }
+
+        final int maxThreadCount = 10;
+        final ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10_000);
+        final ThreadPoolExecutor threadPool = new ThreadPoolExecutor(maxThreadCount,
+                maxThreadCount * 2, 0, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(1024),
+                new ThreadFactoryBuilder().setNameFormat("HW-UP-%d").build(),
+                new ThreadPoolExecutor.CallerRunsPolicy());
+
+        // 产生ID
+        int startId = 30000, imageCount = 10000;
+        threadPool.execute(() -> {
+            IntStream.iterate(startId, i -> i + 1).limit(imageCount).forEach(id -> {
+                try {
+                    queue.put(id);
+                } catch (InterruptedException e) {
+                    log.error(e.getMessage());
+                }
+            });
+        });
+
+
+        final int fileCount = files.length;
+        final String bucketName = "uptang-cloud-test";
+        final ObsClient client = processor.getObsClient();
+
+        LongAdder counter = new LongAdder();
+        IntStream.range(0, maxThreadCount - 1).forEach(idx -> {
+            try {
+                Integer id = queue.take();
+                String fileName = StringUtils.leftPad(String.valueOf(id), 7, '0');
+                String fullName = StringUtils.leftPad(String.valueOf(id % 1000), 3, '0') + '/' + fileName + ".jpg";
+
+                File file = files[id % fileCount];
+                client.putObject(bucketName, fullName, file);
+                counter.add(1);
+
+                log.info("count: {}, file: {}, size: {}", counter.intValue(), fullName, file.length());
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+        });
     }
 }
