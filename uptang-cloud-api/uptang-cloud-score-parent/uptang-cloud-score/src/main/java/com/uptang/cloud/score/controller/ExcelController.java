3bf5ab13528bfcca5791af8605175556739e60bc
Leemyin <lmy@uptang.com.cn>
2019-11-15 15:22:21 +0800

    FiX: 添加单条插入

--- uptang-cloud-api/uptang-cloud-score-parent/uptang-cloud-score/src/main/java/com/uptang/cloud/score/controller/ExcelController.java
+++ uptang-cloud-api/uptang-cloud-score-parent/uptang-cloud-score/src/main/java/com/uptang/cloud/score/controller/ExcelController.java
@@ -1,7 +1,5 @@
 package com.uptang.cloud.score.controller;
 
-import com.uptang.cloud.score.annotation.ArchiveCheck;
-import com.uptang.cloud.score.annotation.JobSwitchCheck;
 import com.uptang.cloud.score.common.converter.ImportFromExcelConverter;
 import com.uptang.cloud.score.common.enums.ScoreTypeEnum;
 import com.uptang.cloud.score.common.vo.ImportFromExcelVo;
@@ -9,11 +7,11 @@ import com.uptang.cloud.score.dto.RequestParameter;
 import com.uptang.cloud.score.service.IExcelDataServiceProcessor;
 import com.uptang.cloud.starter.web.controller.BaseController;
 import com.uptang.cloud.starter.web.domain.ApiOut;
-import io.swagger.annotations.*;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
 
@@ -34,24 +32,22 @@ public class ExcelController extends BaseController {
         this.excelDataServiceProcessor = excelDataServiceProcessor;
     }
 
-    @PostMapping("/{type}/{schoolId}/{gradeId}/{classId}/{semesterId}/{semesterName}")
-    @ArchiveCheck
-    @JobSwitchCheck
+    @PostMapping("/{type}/{schoolId}/{gradeId}/{semesterId}/{semesterName}")
     @ApiOperation(value = "Excel导入成绩", response = String.class)
     @ApiImplicitParams({
             @ApiImplicitParam(name = "type", value = "成绩类型 0 学业成绩 1 体质健康 2 艺术成绩", paramType = "path", required = true),
             @ApiImplicitParam(name = "gradeId", value = "年级ID", paramType = "path", required = true),
-            @ApiImplicitParam(name = "classId", value = "班级ID", paramType = "path", required = true),
             @ApiImplicitParam(name = "schoolId", value = "学校ID", paramType = "path", required = true),
             @ApiImplicitParam(name = "semesterId", value = "学期ID", paramType = "path", required = true),
             @ApiImplicitParam(name = "semesterName", value = "学期名称", paramType = "path", required = true),
+            @ApiImplicitParam(name = "classId", value = "班级ID", paramType = "query"),
     })
     public ApiOut<String> batchInsert(@PathVariable("type") @NotNull Integer type,
                                       @PathVariable("gradeId") @NotNull Long gradeId,
-                                      @PathVariable("classId") @NotNull Long classId,
                                       @PathVariable("schoolId") @NotNull Long schoolId,
                                       @PathVariable("semesterId") @NotNull Long semesterId,
-                                      @PathVariable("semesterName") @NotNull String semesterName) {
+                                      @PathVariable("semesterName") @NotNull String semesterName,
+                                      @RequestParam(value = "classId", required = false) Long classId) {
         ImportFromExcelVo importFromExcel = new ImportFromExcelVo();
         importFromExcel.setSchoolId(schoolId);
         importFromExcel.setScoreType(ScoreTypeEnum.code(type));
